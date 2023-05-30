94bd4b736d8f39bc0cc314ecc29a61c8596e6b8f
gnehgo <gnehgoogheng@gmail.com>
2023-05-30 16:26:26 +0300

    Create Counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import React from "react";
 function App() {
+  const [count,setCount] = React.useState(0);
+  const onClickPlus= () =>{
+     setCount(count+1);
+  }
+  const onCliclMinus= () =>{
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    setCount(count-1);
+  }
   return (
-    <div className="App">
-    </div>
-  );
+      <div ClassName={App}>
+        <div>
+          <h2>Счетсчик:</h2>
+          <h1>{count}</h1>
+          <button onClick={onCliclMinus} className="minus">- Minus</button>
+          <button onClick={onClickPlus} className="Plus">Plus +</button>
+        </div>
+
+      </div>
+  )
 }
 
 export default App;
+
