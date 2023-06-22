61495c0becbaa323ed2aba634f84f5c51c4271c1
gnehgo <gnehgoogheng@gmail.com>
2023-06-22 11:32:46 +0300

    corrective

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherApp from "../components/WeatherApp";
 import "../components/weatherStyle.css"
+import ToDoApp from "../components/ToDoApp";
 
 
 export function ProjectsPage(){
