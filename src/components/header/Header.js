fe2c4d7dea6ef568a77e2c31f87b4929df8e5aa2
gnehgo <gnehgoogheng@gmail.com>
2023-07-08 20:01:42 +0300

    changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,9 +11,6 @@ let Header = () => {
                 </h1>
                 <div className="header_text">
                     <p>with passion for learning and creating.</p>
-                <a  href="#!" className="cvLink">
-                    Download CV
-                </a>
                 </div>
             </div>
         </header>
